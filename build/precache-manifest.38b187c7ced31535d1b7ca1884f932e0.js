9963edabe5cacb4cdbd529121539a74ef7a1149f
Paweł Szymczak <mangoman@wp.pl>
2020-08-26 16:58:11 +0200

    update project

---
+++ build/precache-manifest.38b187c7ced31535d1b7ca1884f932e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2580787a80c224c1db4fffb74e75e5",
+    "url": "/twojadrugawyplata.github.io/currency_coverter_with_API/index.html"
+  },
+  {
+    "revision": "6c8e237894866fb5a2f4",
+    "url": "/twojadrugawyplata.github.io/currency_coverter_with_API/static/css/main.b0274da8.chunk.css"
+  },
+  {
+    "revision": "af10ed883fe4c28fc521",
+    "url": "/twojadrugawyplata.github.io/currency_coverter_with_API/static/js/2.24acf91b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/twojadrugawyplata.github.io/currency_coverter_with_API/static/js/2.24acf91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8e237894866fb5a2f4",
+    "url": "/twojadrugawyplata.github.io/currency_coverter_with_API/static/js/main.d848d243.chunk.js"
+  },
+  {
+    "revision": "2436e1b287c88e1407be",
+    "url": "/twojadrugawyplata.github.io/currency_coverter_with_API/static/js/runtime-main.272c794b.js"
+  },
+  {
+    "revision": "93df400b0e4a3d7c6026de7029fac35e",
+    "url": "/twojadrugawyplata.github.io/currency_coverter_with_API/static/media/background-picture.93df400b.jpg"
+  }
+]);
